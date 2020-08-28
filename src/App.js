bd1e789157110da83007b1513ca22a79de3d36fc
Jean Lohier <jlohier>
2020-08-27 23:12:08 -0400

    adding files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//this line imports react functionality 
+import React from 'react';
+//this line imports the Graph component
+//implemented in a graph.js file which is in the same directory
+import Graph from './graph.js';
+function App() {
+    return (
+        <Graph />
+    );
+}
+export default App;
